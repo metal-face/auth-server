c5f22febf303a8f543a36c211c1ff1f26de56e4f
metal-face <hughesbryan3000@gmail.com>
2024-12-01 20:18:06 -0500

    app state added, create user router built out more

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use axum::{Router, routing::get, routing::post};
+use axum::{Router, routing::get, routing::post, Json};
 use tokio::net::TcpListener;
+use std::sync::Arc;
+use serde_json::json;
+
 mod api;
+mod services;
+
+struct AppState {}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/users", post(api::v1::users::users::create_user()));
+    let shared_state: Arc<AppState> = Arc::new(AppState {});
+    let app: Router = Router::new().route("/users", post({
+        let shared_state: Arc<AppState> = Arc::clone(&shared_state);
+        move |body: Json<api::v1::users::users::CreateUser>| api::v1::users::users::create_user(body, shared_state)
+    }));
 
     let listener: TcpListener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
-
-
-async fn root() {}
