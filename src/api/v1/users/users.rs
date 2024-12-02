422e619beb68cf99f713dbabe2df3d66e7aa59d7
metal-face <hughesbryan3000@gmail.com>
2024-12-01 20:18:45 -0500

    service layer for creating user added

--- src/api/v1/users/users.rs
+++ src/api/v1/users/users.rs
@@ -1,5 +1,33 @@
+use std::sync::Arc;
 use axum::{
     Json,
     extract::{ Extension, Path},
 };
-pub async fn create_user() {}
+use axum::http::StatusCode;
+use serde::Deserialize;
+use serde_json::{Value, json};
+use crate::AppState;
+use crate::services::users::users::validate_user;
+
+#[derive(Deserialize)]
+pub struct CreateUser {
+    email: String,
+    password: String,
+}
+
+pub async fn create_user(Json(payload): Json<CreateUser>, state: Arc<AppState>) -> Json<Value> {
+    let is_valid = validate_user(payload.email, payload.password).await.unwrap();
+
+    if !is_valid {
+        return Json(json!({
+            "status": "error",
+            "message": "Invalid email or password",
+            "status_code": 400
+        }))
+    }
+
+    Json(json!({
+        "status": "success",
+        "status_code": 200
+    }))
+}
