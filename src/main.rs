debd672b5a378df2e5a5d66fc9070c5d5237f6b0
metal-face <hughesbryan3000@gmail.com>
2024-12-01 19:38:28 -0500

    create users route added to router instantiation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use axum::{Router, routing::get};
+use axum::{Router, routing::get, routing::post};
+use tokio::net::TcpListener;
+mod api;
 
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/users", post(api::v1::users::users::create_user()));
 
-fn main() {
-    let app = Router::new().route("/", get(root));
-
+    let listener: TcpListener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
 
