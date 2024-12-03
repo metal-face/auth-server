b751c7155e482ddcc2ead8a128aaaad41efa6f55
metal-face <hughesbryan3000@gmail.com>
2024-12-02 23:24:48 -0500

    rustfmt ran against file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use axum::{Router, routing::get, routing::post, Json};
-use tokio::net::TcpListener;
-use std::sync::Arc;
+use axum::{routing::get, routing::post, Json, Router};
 use serde_json::json;
+use std::sync::Arc;
+use tokio::net::TcpListener;
 
 mod api;
+mod repositories;
 mod services;
 
 struct AppState {}
@@ -11,10 +12,15 @@ struct AppState {}
 #[tokio::main]
 async fn main() {
     let shared_state: Arc<AppState> = Arc::new(AppState {});
-    let app: Router = Router::new().route("/users", post({
-        let shared_state: Arc<AppState> = Arc::clone(&shared_state);
-        move |body: Json<api::v1::users::users::CreateUser>| api::v1::users::users::create_user(body, shared_state)
-    }));
+    let app: Router = Router::new().route(
+        "/users",
+        post({
+            let shared_state: Arc<AppState> = Arc::clone(&shared_state);
+            move |body: Json<api::v1::users::users::CreateUser>| {
+                api::v1::users::users::create_user(body, shared_state)
+            }
+        }),
+    );
 
     let listener: TcpListener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
     axum::serve(listener, app).await.unwrap();
