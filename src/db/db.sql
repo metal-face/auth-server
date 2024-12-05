9f0801a939689b22bab87bf527c7126a93f6c474
metal-face <hughesbryan3000@gmail.com>
2024-12-04 20:01:34 -0500

    db schema file written

---
+++ src/db/db.sql
@@ -0,0 +1,17 @@
+create extension "uuid-ossp";
+
+CREATE TABLE users (
+    id uuid primary key default uuid_generate_v4(),
+    first_name text not null,
+    last_name text not null,
+    email text not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+    deleted_at timestamp
+);
+
+CREATE TABLE application (
+    id uuid primary key default uuid_generate_v4(),
+    application_name text not null,
+    user_id uuid not null references users(id)
+);
