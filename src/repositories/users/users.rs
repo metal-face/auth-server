f7db27daaa085a6ab57faf35866b2a6643a5eec4
metal-face <hughesbryan3000@gmail.com>
2024-12-05 23:41:11 -0500

    anyhow response utilized

--- src/repositories/users/users.rs
+++ src/repositories/users/users.rs
@@ -3,23 +3,24 @@ use sqlx::postgres::PgPool;
 use sqlx::types::Json;
 use sqlx::{query, Error};
 
+#[derive(Default)]
 pub struct User {
-    first_name: String,
-    last_name: String,
-    email: String,
-    password: Option<String>,
-    created_at: DateTime<Local>,
-    updated_at: DateTime<Local>,
-    deleted_at: Option<DateTime<Local>>,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub password: Option<String>,
+    pub created_at: DateTime<Local>,
+    pub updated_at: DateTime<Local>,
+    pub deleted_at: Option<DateTime<Local>>,
 }
 
-pub async fn create_user(pool: &PgPool, user: User) -> Result<User, Error> {
+pub async fn create_user(pool: &PgPool, user: User) -> anyhow::Result<User> {
     let result: User = query!(
         r#"INSERT INTO users ( user ) VALUES ( $1 ) RETURNING *"#,
-        Json(user) as _ 
+        Json(user) as _
     )
     .fetch_one(pool)
     .await?;
 
-    return Ok(result);
+    Ok(result)
 }
