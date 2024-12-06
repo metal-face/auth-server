df6f047e4b84e6031c53ab8ddb6629b8ab514620
metal-face <hughesbryan3000@gmail.com>
2024-12-05 22:52:54 -0500

    db pool added to app state, route config simplified

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use axum::{routing::get, routing::post, Json, Router};
-use serde_json::json;
+use crate::api::v1::users::users::{create_user, CreateUser};
+use axum::{routing::post, Json, Router};
 use sqlx::postgres::PgPoolOptions;
-use sqlx::Postgres;
-use sqlx_core::pool::Pool;
+use sqlx::PgPool;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::net::TcpListener;
@@ -11,31 +10,25 @@ mod api;
 mod repositories;
 mod services;
 
-struct AppState {}
+struct AppState {
+    db: PgPool,
+}
 
 #[tokio::main]
 async fn main() {
     let db_connection_str: String = std::env::var("DATABASE_URL")
         .unwrap_or_else(|_| "postgres://metalface:password@localhost".to_string());
 
-    let pool: Pool<Postgres> = PgPoolOptions::new()
+    let pool: PgPool = PgPoolOptions::new()
         .max_connections(5)
         .acquire_timeout(Duration::from_secs(5))
         .connect(&db_connection_str)
         .await
         .expect("Can't connect to database");
 
-    let shared_state: Arc<AppState> = Arc::new(AppState {});
+    let shared_state: Arc<AppState> = Arc::new(AppState { db: pool });
 
-    let app: Router = Router::new().route(
-        "/users",
-        post({
-            let shared_state: Arc<AppState> = Arc::clone(&shared_state);
-            move |body: Json<api::v1::users::users::CreateUser>| {
-                api::v1::users::users::create_user(body, shared_state)
-            }
-        }),
-    );
+    let app: Router = Router::new().route("/users", post(create_user));
 
     let listener: TcpListener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
     axum::serve(listener, app).await.unwrap();
