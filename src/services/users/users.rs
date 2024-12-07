82ffae0f1c9e0c406bbf0ba1e75644d678de5114
metal-face <hughesbryan3000@gmail.com>
2024-12-06 20:38:41 -0500

    repository layer called from service layer

--- src/services/users/users.rs
+++ src/services/users/users.rs
@@ -1,8 +1,9 @@
-use crate::repositories::users::users::User;
+use crate::repositories::users::users::{create_user, User};
 use chrono::{DateTime, Local};
 use sqlx::PgPool;
 
-pub async fn validate_user(email: String, password: String, pool: &PgPool) -> anyhow::Result<User> {
-    let user = Default::default();
-    Ok(user)
+pub async fn validate_user(user: User, pool: &PgPool) -> anyhow::Result<User> {
+    let res = create_user(pool, user).await?;
+
+    Ok(res)
 }
