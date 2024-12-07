920a89097fe392ec2fc0227347772d8c73fe4140
metal-face <hughesbryan3000@gmail.com>
2024-12-07 11:02:32 -0500

    anyhow Error added to return signature

--- src/services/users/users.rs
+++ src/services/users/users.rs
@@ -1,7 +1,7 @@
 use crate::repositories::users::users::{create_user, User};
 use sqlx::PgPool;
 
-pub async fn validate_user(user: User, pool: &PgPool) -> anyhow::Result<User> {
+pub async fn validate_user(user: User, pool: &PgPool) -> anyhow::Result<User, anyhow::Error> {
     let res = create_user(pool, user).await?;
 
     Ok(res)
