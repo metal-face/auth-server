e38dd5ca422e4f39e633b2ddf44eefc0f1ffb2c3
metal-face <hughesbryan3000@gmail.com>
2024-12-06 20:38:23 -0500

    user constructed from json payload, passed to service layer

--- src/api/v1/users/users.rs
+++ src/api/v1/users/users.rs
@@ -1,24 +1,42 @@
+use crate::repositories::users::users::User;
 use crate::services::users::users::validate_user;
 use crate::AppState;
-use anyhow_http::response::{HttpJsonResult, HttpResult};
 use axum::extract::State;
 use axum::response::IntoResponse;
 use axum::Json;
+use chrono::Local;
 use serde::Deserialize;
-use serde_json::{json, Value};
+use serde_json::json;
 use std::sync::Arc;
 
 #[derive(Deserialize)]
 pub struct CreateUser {
+    first_name: String,
+    last_name: String,
     email: String,
     password: String,
 }
 
 pub async fn create_user(
-    Json(CreateUser { email, password }): Json<CreateUser>,
+    Json(CreateUser {
+        first_name,
+        last_name,
+        email,
+        password,
+    }): Json<CreateUser>,
     State(state): State<Arc<AppState>>,
 ) -> impl IntoResponse {
-    match validate_user(email, password, &state.db) {
+    let user = User {
+        first_name,
+        last_name,
+        email,
+        password: Some(password),
+        created_at: Local::now(),
+        updated_at: Local::now(),
+        deleted_at: None,
+    };
+
+    match validate_user(user, &state.db) {
         Ok(user) => json!({ "status": 200, "user": user}),
         Err(err) => json!({ "error": err.to_string()}),
     }
