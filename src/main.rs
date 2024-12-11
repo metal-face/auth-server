de24a299e7924c03221d479d11ce12ebfb875a46
metal-face <hughesbryan3000@gmail.com>
2024-12-10 22:56:14 -0500

    bug fixes

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::sync::Arc;
 use std::time::Duration;
-use tokio::net::TcpListener;
 
 mod api;
 mod repositories;
@@ -16,22 +15,21 @@ struct AppState {
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let db_connection_str: String = std::env::var("DATABASE_URL")
-        .unwrap_or_else(|_| "postgres://metalface:password@localhost".to_string());
+    let db_connection_str = String::from("jdbc:postgresql://localhost:5432/postgres");
 
-    let pool: PgPool = PgPoolOptions::new()
+    let pool = PgPoolOptions::new()
         .max_connections(5)
         .acquire_timeout(Duration::from_secs(5))
         .connect(&db_connection_str)
         .await
         .expect("Can't connect to database");
 
-    let shared_state: Arc<AppState> = Arc::new(AppState { db: pool });
+    let shared_state = Arc::new(AppState { db: pool });
 
-    let app: Router = Router::new()
+    let app = Router::new()
         .route("/users", post(create_user))
         .with_state(shared_state);
 
-    let listener: TcpListener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
