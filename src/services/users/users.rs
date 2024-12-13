fc52a46b0416e891b73e0ceb6cc015f093ccb44c
metal-face <hughesbryan3000@gmail.com>
2024-12-12 20:18:36 -0500

    bug fix - reference to value used as to not move user value

--- src/services/users/users.rs
+++ src/services/users/users.rs
@@ -4,8 +4,16 @@ use email_address::EmailAddress;
 use sqlx::PgPool;
 
 pub async fn validate_user(user: User, pool: &PgPool) -> anyhow::Result<User, anyhow::Error> {
-    if user.password.unwrap().chars().count() < 8 {
-        bail!("User password must be at least 8 characters");
+    let User {
+        ref first_name,
+        ref last_name,
+        ..
+    } = user;
+
+    if let Some(password) = &user.password {
+        if password.chars().count() < 8 {
+            bail!("User password must be at least 8 characters");
+        }
     }
 
     let is_valid_email_address = EmailAddress::is_valid(&user.email);
@@ -14,11 +22,11 @@ pub async fn validate_user(user: User, pool: &PgPool) -> anyhow::Result<User, an
         bail!("Invalid email address");
     }
 
-    if user.first_name.chars().count() < 3 || user.first_name.chars().count() > 24 {
+    if first_name.chars().count() < 3 || first_name.chars().count() > 24 {
         bail!("User first name length is invalid");
     }
 
-    if user.last_name.chars().count() < 3 || user.last_name.chars().count() > 24 {
+    if last_name.chars().count() < 3 || last_name.chars().count() > 24 {
         bail!("User last name length is invalid");
     }
 
