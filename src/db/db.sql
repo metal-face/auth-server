8bd13d1254a44c11d1441229b58c3d96f3f836fe
metal-face <hughesbryan3000@gmail.com>
2024-12-12 20:18:15 -0500

    password field added

--- src/db/db.sql
+++ src/db/db.sql
@@ -1,17 +1,20 @@
 create extension "uuid-ossp";
 
-CREATE TABLE users (
-    id uuid primary key default uuid_generate_v4(),
-    first_name text not null,
-    last_name text not null,
-    email text not null,
+CREATE TABLE users
+(
+    id         uuid primary key   default uuid_generate_v4(),
+    first_name text      not null,
+    last_name  text      not null,
+    email      text      not null,
+    password   text      not null,
     created_at timestamp not null default now(),
     updated_at timestamp not null default now(),
     deleted_at timestamp
 );
 
-CREATE TABLE application (
-    id uuid primary key default uuid_generate_v4(),
+CREATE TABLE application
+(
+    id               uuid primary key default uuid_generate_v4(),
     application_name text not null,
-    user_id uuid not null references users(id)
+    user_id          uuid not null references users (id)
 );
