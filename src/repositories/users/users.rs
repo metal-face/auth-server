5c1f854713157fcf564920e9aa33b4653d7c1a83
metal-face <hughesbryan3000@gmail.com>
2024-12-16 17:44:07 -0500

    password verification logic added

--- src/repositories/users/users.rs
+++ src/repositories/users/users.rs
@@ -1,6 +1,8 @@
+use argon2::{Argon2, PasswordHasher};
+use axum::response::IntoResponse;
 use chrono::{DateTime, Local};
 use password_hash::rand_core::OsRng;
-use password_hash::{Salt, SaltString};
+use password_hash::{PasswordHash, PasswordVerifier, Salt, SaltString};
 use serde::Serialize;
 use sqlx::postgres::PgPool;
 use sqlx::query;
@@ -16,11 +18,25 @@ pub struct User {
     pub deleted_at: Option<DateTime<Local>>,
 }
 
+fn verify_password(password: &str, hash: PasswordHash) -> Result<bool, bool> {
+    match Argon2::default().verify_password(password.as_bytes(), &hash) {
+        Ok(_) => Ok(true),
+        Err(_) => Err(false),
+    }
+}
+
 pub async fn create_user(pool: &PgPool, user: User) -> anyhow::Result<User, anyhow::Error> {
     let Some(password) = &user.password;
 
     let salt_string = SaltString::generate(&mut OsRng);
     let salt: Salt = salt_string.try_into()?;
+
+    let argon2 = Argon2::default();
+    let hash = argon2.hash_password(password.as_bytes(), salt).unwrap();
+
+    Argon2::default()
+        .verify_password(password.as_bytes(), &hash)
+        .expect("Invalid Password");
 
     query(
             "INSERT INTO users ( first_name, last_name, email, password, created_at, updated_at ) VALUES ( $1, $2, $3, $4, $5, $6 ) RETURNING *",
