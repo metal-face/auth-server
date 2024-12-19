8de825ad1a7cfdce84b45e6ee60bed63051597c3
metal-face <hughesbryan3000@gmail.com>
2024-12-19 18:20:50 -0500

    logic for password verification added

--- src/services/authenticate/authenticate.rs
+++ src/services/authenticate/authenticate.rs
@@ -1,5 +1,6 @@
-use crate::repositories::users::users::User;
-use chrono::Local;
+use crate::repositories::users::users::{get_user_by_email, User};
+use argon2::Argon2;
+use password_hash::{PasswordHash, PasswordVerifier};
 use sqlx::PgPool;
 
 pub async fn validate_user_credentials(
@@ -7,14 +8,15 @@ pub async fn validate_user_credentials(
     password: String,
     pool: &PgPool,
 ) -> anyhow::Result<User> {
-    let temp = User {
-        first_name: String::from(""),
-        last_name: String::from(""),
-        email: String::from(""),
-        password: Default::default(),
-        created_at: Local::now(),
-        updated_at: Local::now(),
-        deleted_at: None,
-    };
-    Ok(temp)
+    let db_user = get_user_by_email(pool, &email).await?;
+    let password_hash = PasswordHash::new(&db_user.hashed_password);
+    let is_okay = Argon2::default()
+        .verify_password(password.as_bytes(), &password_hash.unwrap())
+        .is_ok();
+
+    if is_okay {
+        Ok(db_user)
+    } else {
+        Err(anyhow::anyhow!("Error verifying password!"))
+    }
 }
