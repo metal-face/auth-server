34bc701dfa9aff4fd04d8d9b720d142d4db94517
metal-face <hughesbryan3000@gmail.com>
2024-12-19 18:18:04 -0500

    bug fix: data type for dates incorrect, email made unique

--- src/db/db.sql
+++ src/db/db.sql
@@ -2,15 +2,17 @@ create extension "uuid-ossp";
 
 CREATE TABLE users
 (
-    id              uuid primary key   default uuid_generate_v4(),
-    first_name      text      not null,
-    last_name       text      not null,
-    email           text      not null,
-    hashed_password text      not null,
-    created_at      timestamp not null default now(),
-    updated_at      timestamp not null default now(),
-    deleted_at      timestamp
+    id              uuid primary key     default uuid_generate_v4(),
+    first_name      text        not null,
+    last_name       text        not null,
+    email           text unique not null,
+    hashed_password text        not null,
+    created_at      timestamptz not null default now(),
+    updated_at      timestamptz not null default now(),
+    deleted_at      timestamptz
 );
+
+CREATE INDEX users_email ON users (email);
 
 CREATE TABLE application
 (
