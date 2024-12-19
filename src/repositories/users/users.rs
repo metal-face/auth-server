6848c1d1691ceb1afbd86e518cf38a78d9354fb9
metal-face <hughesbryan3000@gmail.com>
2024-12-19 18:19:52 -0500

    fields enumerated instead of selecting all

--- src/repositories/users/users.rs
+++ src/repositories/users/users.rs
@@ -4,35 +4,33 @@ use password_hash::rand_core::OsRng;
 use password_hash::{PasswordHash, PasswordVerifier, Salt, SaltString};
 use serde::Serialize;
 use sqlx::postgres::PgPool;
-use sqlx::query;
+use sqlx::{query, query_as};
 
 #[derive(sqlx::FromRow, Serialize)]
 pub struct User {
     pub first_name: String,
     pub last_name: String,
     pub email: String,
-    pub password: Option<String>,
+    pub hashed_password: String,
     pub created_at: DateTime<Local>,
     pub updated_at: DateTime<Local>,
     pub deleted_at: Option<DateTime<Local>>,
 }
 
-fn verify_password(password: &str, hash: PasswordHash) -> bool {
+pub fn verify_password(password: &str, hash: PasswordHash) -> bool {
     Argon2::default()
         .verify_password(password.as_bytes(), &hash)
         .is_ok()
 }
 
 pub async fn create_user(pool: &PgPool, user: User) -> anyhow::Result<User, anyhow::Error> {
-    let Some(password) = &user.password else {
-        panic!()
-    };
-
     let salt_string = SaltString::generate(&mut OsRng);
     let salt: Salt = salt_string.as_salt();
 
     let argon2 = Argon2::default();
-    let hash = argon2.hash_password(password.as_bytes(), salt).unwrap();
+    let hash = argon2
+        .hash_password(user.hashed_password.as_bytes(), salt)
+        .unwrap();
 
     query(
             "INSERT INTO users ( first_name, last_name, email, hashed_password, created_at, updated_at ) VALUES ( $1, $2, $3, $4, $5, $6 ) RETURNING *",
@@ -50,10 +48,10 @@ pub async fn create_user(pool: &PgPool, user: User) -> anyhow::Result<User, anyh
 }
 
 pub async fn get_user_by_email(pool: &PgPool, email: &str) -> Result<User, anyhow::Error> {
-    let result = query("SELECT * FROM users WHERE email = $1")
+    let result = query_as::<_, User>("SELECT u.first_name, u.last_name, u.email, u.hashed_password, u.created_at, u.updated_at, u.deleted_at FROM users as u WHERE email = $1")
         .bind(email)
-        .execute(pool)
+        .fetch_one(pool)
         .await?;
 
-    // result.Ok(user)
+    Ok(result)
 }
