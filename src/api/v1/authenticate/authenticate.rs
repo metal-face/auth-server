391fa0bd684123539b38ea011ff7c12ebcba03bb
metal-face <hughesbryan3000@gmail.com>
2024-12-19 18:21:24 -0500

    controller written

--- src/api/v1/authenticate/authenticate.rs
+++ src/api/v1/authenticate/authenticate.rs
@@ -1,16 +1,31 @@
+use crate::services::authenticate::authenticate::validate_user_credentials;
 use crate::AppState;
 use axum::extract::State;
-use axum::response::IntoResponse;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
 use axum::Json;
+use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 
+#[derive(Serialize, Deserialize)]
 pub struct Credentials {
     email: String,
     password: String,
 }
 
+#[axum::debug_handler]
 pub async fn log_in(
     State(state): State<Arc<AppState>>,
     Json(Credentials { email, password }): Json<Credentials>,
 ) -> impl IntoResponse {
+    match validate_user_credentials(email, password, &state.db).await {
+        Ok(user) => Ok(Response::builder()
+            .status(StatusCode::OK)
+            .body(Json(user).into_response())
+            .unwrap()),
+        Err(err) => Err(Response::builder()
+            .status(StatusCode::UNAUTHORIZED)
+            .body(err.to_string().into_response())
+            .unwrap()),
+    }
 }
