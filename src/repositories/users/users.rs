b7274396342d150b5e04ed17ef5c924341d510bd
metal-face <hughesbryan3000@gmail.com>
2024-12-20 20:31:48 -0500

    create user function adjusted

--- src/repositories/users/users.rs
+++ src/repositories/users/users.rs
@@ -1,13 +1,15 @@
+use crate::api::v1::users::users::UserDTO;
 use argon2::{Argon2, PasswordHasher};
 use chrono::{DateTime, Local};
 use password_hash::rand_core::OsRng;
 use password_hash::{PasswordHash, PasswordVerifier, Salt, SaltString};
 use serde::Serialize;
 use sqlx::postgres::PgPool;
-use sqlx::{query, query_as};
+use sqlx::{query, query_as, Row};
 
 #[derive(sqlx::FromRow, Serialize)]
 pub struct User {
+    pub id: String,
     pub first_name: String,
     pub last_name: String,
     pub email: String,
@@ -23,17 +25,17 @@ pub fn verify_password(password: &str, hash: PasswordHash) -> bool {
         .is_ok()
 }
 
-pub async fn create_user(pool: &PgPool, user: User) -> anyhow::Result<User, anyhow::Error> {
+pub async fn create_user(pool: &PgPool, user: UserDTO) -> anyhow::Result<User, anyhow::Error> {
     let salt_string = SaltString::generate(&mut OsRng);
     let salt: Salt = salt_string.as_salt();
 
     let argon2 = Argon2::default();
     let hash = argon2
-        .hash_password(user.hashed_password.as_bytes(), salt)
+        .hash_password(user.password.as_bytes(), salt)
         .unwrap();
 
-    query(
-            "INSERT INTO users ( first_name, last_name, email, hashed_password, created_at, updated_at ) VALUES ( $1, $2, $3, $4, $5, $6 ) RETURNING *",
+    let result = query(
+            "INSERT INTO users ( first_name, last_name, email, hashed_password, created_at, updated_at ) VALUES ( $1, $2, $3, $4, $5, $6 ) RETURNING (id, first_name, last_name, email, hashed_password, created_at, updated_at, deleted_at)",
         )
             .bind(user.first_name.clone())
             .bind(user.last_name.clone())
@@ -41,8 +43,19 @@ pub async fn create_user(pool: &PgPool, user: User) -> anyhow::Result<User, anyh
             .bind(hash.serialize().to_string())
             .bind(user.created_at.clone())
             .bind(user.updated_at.clone())
-            .execute(pool)
+            .fetch_one(pool)
             .await?;
+
+    let user = User {
+        id: result.get("id"),
+        first_name: result.get("first_name"),
+        last_name: result.get("last_name"),
+        email: result.get("email"),
+        hashed_password: result.get("hashed_password"),
+        created_at: result.get("created_at"),
+        updated_at: result.get("updated_at"),
+        deleted_at: result.get("deleted_at"),
+    };
 
     Ok(user)
 }
