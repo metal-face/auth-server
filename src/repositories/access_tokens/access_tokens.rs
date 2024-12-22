22adea479f59db07c13320f3ffe3cd9c6db8aec9
metal-face <hughesbryan3000@gmail.com>
2024-12-21 20:32:57 -0500

    access token returned

--- src/repositories/access_tokens/access_tokens.rs
+++ src/repositories/access_tokens/access_tokens.rs
@@ -1,27 +1,39 @@
 use chrono::{DateTime, Local};
-use sqlx::{query, PgPool};
+use serde::{Deserialize, Serialize};
+use sqlx::{query, PgPool, Row};
 
-struct AccessToken {
-    id: String,
-    token: String,
-    user_id: String,
-    created_at: DateTime<Local>,
-    updated_at: DateTime<Local>,
-    expires_at: DateTime<Local>,
+#[derive(Deserialize, Serialize, sqlx::FromRow)]
+pub struct AccessToken {
+    pub id: String,
+    pub token: String,
+    pub user_id: String,
+    pub created_at: DateTime<Local>,
+    pub updated_at: DateTime<Local>,
+    pub expires_at: DateTime<Local>,
 }
 
 pub async fn create_access_token(
-    access_token: &String,
+    token: &String,
     user_id: String,
     expires_at: DateTime<Local>,
     pool: &PgPool,
-) -> anyhow::Result<bool> {
-    query("INSERT INTO access_tokens (token, user_id, expires_at) VALUES ($1, $2, $3)")
-        .bind(access_token)
-        .bind(user_id)
-        .bind(expires_at)
-        .execute(pool)
-        .await?;
+) -> anyhow::Result<AccessToken, anyhow::Error> {
+    let result =
+        query("INSERT INTO access_tokens (user_id, token, expires_at) VALUES ($1, $2, $3)")
+            .bind(user_id)
+            .bind(token)
+            .bind(expires_at)
+            .fetch_one(pool)
+            .await?;
 
-    Ok(true)
+    let access_token = AccessToken {
+        id: result.get("id"),
+        user_id: result.get("user_id"),
+        token: result.get("token"),
+        created_at: result.get("created_at"),
+        updated_at: result.get("updated_at"),
+        expires_at: result.get("expires_at"),
+    };
+
+    Ok(access_token)
 }
