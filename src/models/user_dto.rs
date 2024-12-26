97f5ac1bf1ae30b322a3c416506e2b32dea7edab
metal-face <hughesbryan3000@gmail.com>
2024-12-25 23:27:07 -0500

    optionl id

--- src/models/user_dto.rs
+++ src/models/user_dto.rs
@@ -1,8 +1,10 @@
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize)]
 pub struct UserDTO {
+    pub id: Option<Uuid>,
     pub first_name: String,
     pub last_name: String,
     pub email: String,
