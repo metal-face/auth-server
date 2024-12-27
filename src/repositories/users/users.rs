90a01b5532a5d759dad0e58e9839a06913f59fd7
metal-face <hughesbryan3000@gmail.com>
2024-12-27 03:17:16 -0500

    new function called, get_user_by_id written

--- src/repositories/users/users.rs
+++ src/repositories/users/users.rs
@@ -1,10 +1,12 @@
 use crate::models::user::User;
 use crate::models::user_dto::UserDTO;
+use crate::utilities::generate_user::generate_user;
 use argon2::{Argon2, PasswordHasher};
 use password_hash::rand_core::OsRng;
 use password_hash::{Salt, SaltString};
 use sqlx::postgres::PgPool;
 use sqlx::{query, query_as, Row};
+use uuid::Uuid;
 
 pub async fn create_user(pool: &PgPool, user: UserDTO) -> anyhow::Result<User, anyhow::Error> {
     let password = user.password.unwrap_or_default();
@@ -15,7 +17,7 @@ pub async fn create_user(pool: &PgPool, user: UserDTO) -> anyhow::Result<User, a
     let hash = argon2.hash_password(password.as_bytes(), salt).unwrap();
 
     let result = query(
-            "INSERT INTO users ( first_name, last_name, email, hashed_password) VALUES ( $1, $2, $3, $4 ) RETURNING id, first_name, last_name, email, hashed_password, created_at, updated_at, deleted_at",
+            "INSERT INTO users ( first_name, last_name, email, hashed_password ) VALUES ( $1, $2, $3, $4 ) RETURNING id, first_name, last_name, email, hashed_password, created_at, updated_at, deleted_at",
         )
             .bind(user.first_name.clone())
             .bind(user.last_name.clone())
@@ -24,16 +26,16 @@ pub async fn create_user(pool: &PgPool, user: UserDTO) -> anyhow::Result<User, a
             .fetch_one(pool)
             .await?;
 
-    let user = User {
-        id: result.get("id"),
-        first_name: result.get("first_name"),
-        last_name: result.get("last_name"),
-        email: result.get("email"),
-        hashed_password: result.get("hashed_password"),
-        created_at: result.get("created_at"),
-        updated_at: result.get("updated_at"),
-        deleted_at: result.get("deleted_at"),
-    };
+    let user = generate_user(
+        result.get("id"),
+        result.get("first_name"),
+        result.get("last_name"),
+        result.get("email"),
+        result.get("hashed_password"),
+        result.get("created_at"),
+        result.get("updated_at"),
+        result.get("deleted_at"),
+    );
 
     Ok(user)
 }
@@ -41,6 +43,15 @@ pub async fn create_user(pool: &PgPool, user: UserDTO) -> anyhow::Result<User, a
 pub async fn get_user_by_email(pool: &PgPool, email: &str) -> Result<User, anyhow::Error> {
     let result = query_as::<_, User>("SELECT u.id as id, u.first_name, u.last_name, u.email, u.hashed_password, u.created_at, u.updated_at, u.deleted_at FROM users as u WHERE email = $1")
         .bind(email)
+        .fetch_one(pool)
+        .await?;
+
+    Ok(result)
+}
+
+pub async fn get_user_by_id(pool: &PgPool, id: Uuid) -> Result<User, anyhow::Error> {
+    let result = query_as::<_, User>("SELECT u.id, u.first_name, u.last_name, u.email, u.hashed_password, u.created_at, u.updated_at, u.deleted_at FROM users as u WHERE id = $1")
+        .bind(id)
         .fetch_one(pool)
         .await?;
 
