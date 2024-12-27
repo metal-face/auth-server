e6bd0f288f11df7c11bde5570c62dc4c4de7d7e2
metal-face <hughesbryan3000@gmail.com>
2024-12-27 03:34:50 -0500

    struct renamed

--- src/models/claims.rs
+++ src/models/claims.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Claims {
+pub struct JWTClaims {
     pub jti: String,
     pub iss: String,
     pub aud: String,
