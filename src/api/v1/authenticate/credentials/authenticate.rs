a13d075f93a8c9c5e85d0086fbb351efce83f95c
metal-face <hughesbryan3000@gmail.com>
2024-12-27 03:35:18 -0500

    jwt generation logic abstracted into function

--- src/api/v1/authenticate/credentials/authenticate.rs
+++ src/api/v1/authenticate/credentials/authenticate.rs
@@ -1,19 +1,12 @@
-use crate::repositories::access_tokens::access_tokens::create_access_token;
-use crate::services::authenticate::authenticate::validate_user_credentials;
-use crate::models::claims::Claims;
 use crate::models::credentials::Credentials;
+use crate::services::authenticate::authenticate::validate_user_credentials;
+use crate::utilities::mint_jwt::mint_jwt;
 use crate::AppState;
 use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::Json;
-use chrono::DateTime;
-use jsonwebtoken::{encode, EncodingKey, Header};
-use std::env;
 use std::sync::Arc;
-use std::time::{SystemTime, UNIX_EPOCH};
-use uuid::Uuid;
-
 
 #[axum::debug_handler]
 pub async fn log_in(
@@ -22,37 +15,7 @@ pub async fn log_in(
 ) -> impl IntoResponse {
     match validate_user_credentials(email, password, &state.db).await {
         Ok(user) => {
-            let in_at = SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .unwrap()
-                .as_secs();
-
-            let date_time = DateTime::from_timestamp((in_at + 1_209_600) as i64, 0);
-            let expires_at = DateTime::from(date_time.unwrap());
-
-            let my_claims = Claims {
-                jti: Uuid::new_v4().to_string(),
-                aud: "client".to_owned(),
-                sub: user.email.to_owned(),
-                iss: "auth_server".to_owned(),
-                first_name: user.first_name.to_owned(),
-                last_name: user.last_name.to_owned(),
-                client_id: Uuid::new_v4().to_string(),
-                iat: in_at,
-                exp: in_at + 1_209_600,
-            };
-
-            let jwt = encode(
-                &Header::default(),
-                &my_claims,
-                &EncodingKey::from_secret(env::var("PRIVATE_KEY").unwrap().as_ref()),
-            )
-            .unwrap();
-
-            create_access_token(&jwt, user.id, expires_at, &state.db)
-                .await
-                .unwrap();
-
+            let jwt = mint_jwt(&user, &state.db);
 
             Ok(Response::builder()
                 .status(StatusCode::OK)
