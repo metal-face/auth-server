49e9e06406126c38d40191865ff0513b6a611063
metal-face <hughesbryan3000@gmail.com>
2024-12-28 20:15:14 -0500

    user from method called instead of generate_user_dto

--- src/api/v1/users/get_user_by_id.rs
+++ src/api/v1/users/get_user_by_id.rs
@@ -1,5 +1,5 @@
+use crate::models::user_dto::UserDTO;
 use crate::repositories::users::users::get_user_by_id;
-use crate::utilities::generate_user_dto::generate_user_dto;
 use crate::AppState;
 use axum::extract::{Path, State};
 use axum::http::{Response, StatusCode};
@@ -15,16 +15,8 @@ pub async fn get_user(
 ) -> impl IntoResponse {
     match get_user_by_id(&state.db, Uuid::from_str(&user_id).unwrap()).await {
         Ok(user) => {
-            let user_dto = generate_user_dto(
-                Some(user.id),
-                user.first_name,
-                user.last_name,
-                user.email,
-                None,
-                Some(user.created_at),
-                Some(user.updated_at),
-                user.deleted_at,
-            );
+            let user_dto = UserDTO::from(user);
+
             Ok(Response::builder()
                 .status(StatusCode::OK)
                 .body(Json(user_dto).into_response())
