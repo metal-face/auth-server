5662be224e7ac049189e4c68539873056220803d
metal-face <hughesbryan3000@gmail.com>
2024-12-28 20:15:24 -0500

    from method written

--- src/models/user_dto.rs
+++ src/models/user_dto.rs
@@ -1,3 +1,4 @@
+use crate::models::user::User;
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -12,4 +13,19 @@ pub struct UserDTO {
     pub created_at: Option<DateTime<Local>>,
     pub updated_at: Option<DateTime<Local>>,
     pub deleted_at: Option<DateTime<Local>>,
+}
+
+impl From<User> for UserDTO {
+    fn from(value: User) -> Self {
+        UserDTO {
+            id: Some(value.id),
+            first_name: value.first_name,
+            last_name: value.last_name,
+            email: value.email,
+            password: None,
+            created_at: Some(value.created_at),
+            updated_at: Some(value.updated_at),
+            deleted_at: value.deleted_at,
+        }
+    }
 }
