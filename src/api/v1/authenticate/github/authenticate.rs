e9d06eb40725134c252ee4d1aeb1e6ee9fcfefae
metal-face <hughesbryan3000@gmail.com>
2025-01-05 05:36:12 -0500

    callback written

--- src/api/v1/authenticate/github/authenticate.rs
+++ src/api/v1/authenticate/github/authenticate.rs
@@ -1,6 +1,50 @@
-use axum::http::{Response, StatusCode};
-use axum::response::IntoResponse;
+use axum::extract::Query;
+use axum::http::StatusCode;
+use axum::Json;
+use std::env;
 
-pub async fn github_authentication() -> impl IntoResponse {
-    Response::builder().status(StatusCode::ACCEPTED).body(().into_response()).unwrap();
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    code: String,
+}
+
+#[derive(serde::Serialize)]
+struct GitHubRequest {
+    client_id: String,
+    client_secret: String,
+    code: String,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct GitHubResponse {
+    access_token: String,
+    token_type: String,
+    scope: String,
+}
+
+pub async fn github_authentication(query_params: Query<QueryParams>) -> StatusCode {
+    let client_id = env::var("GITHUB_CLIENT_ID").unwrap();
+    let client_secret = env::var("GITHUB_CLIENT_SECRET").unwrap();
+    let code = query_params.code.clone();
+
+    let body: GitHubRequest = GitHubRequest {
+        client_id,
+        client_secret,
+        code,
+    };
+
+    let req = reqwest::Client::new()
+        .post("https://github.com/login/oauth/access_token")
+        .header("Accept", "application/json")
+        .json(&body)
+        .send()
+        .await
+        .unwrap()
+        .json::<GitHubResponse>()
+        .await
+        .unwrap();
+
+    println!("{:?}", req);
+
+    StatusCode::ACCEPTED
 }
